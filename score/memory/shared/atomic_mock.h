dcb6102119b799a40b3fb77594327ed506713ac7
Krishna Satish Deshkulkarni Shankaranarayana <krishna-satish.deshkulkarni-shankaranarayana@bmw.de>
2026-03-24 10:22:37 +0100

    refactor(concurrency): introduce atomic utilities module
    - Move atomic_indirector from memory/shared to concurrency/atomic
    - Move atomic_mock from memory/shared to concurrency/atomic
    - Move i_atomic interface from memory/shared to concurrency/atomic
    - Create dedicated atomic module in concurrency

--- score/memory/shared/atomic_mock.h
+++ score/memory/shared/atomic_mock.h
@@ -1,41 +1,21 @@
-/********************************************************************************
- * Copyright (c) 2025 Contributors to the Eclipse Foundation
- *
- * See the NOTICE file(s) distributed with this work for additional
- * information regarding copyright ownership.
- *
- * This program and the accompanying materials are made available under the
- * terms of the Apache License Version 2.0 which is available at
- * https://www.apache.org/licenses/LICENSE-2.0
- *
- * SPDX-License-Identifier: Apache-2.0
- ********************************************************************************/
-#ifndef SCORE_LIB_MEMORY_SHARED_MOCK_ATOMICMOCK_H
-#define SCORE_LIB_MEMORY_SHARED_MOCK_ATOMICMOCK_H
+// *******************************************************************************
+// Copyright (c) 2025 Contributors to the Eclipse Foundation
+//
+// See the NOTICE file(s) distributed with this work for additional
+// information regarding copyright ownership.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Apache License Version 2.0 which is available at
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// SPDX-License-Identifier: Apache-2.0
+// *******************************************************************************
 
-#include "score/memory/shared/i_atomic.h"
+#ifndef SCORE_MEMORY_SHARED_ATOMIC_MOCK_H
+#define SCORE_MEMORY_SHARED_ATOMIC_MOCK_H
 
-#include "gmock/gmock.h"
+#warning "score/memory/shared/atomic_mock.h is deprecated. Include score/concurrency/atomic/atomic_mock.h instead."
 
-#include <atomic>
+#include "score/concurrency/atomic/atomic_mock.h"
 
-namespace score::memory::shared
-{
-
-template <typename T>
-class AtomicMock final : public IAtomic<T>
-{
-  public:
-    MOCK_METHOD(T, fetch_add, (T, std::memory_order), (noexcept, override));
-    MOCK_METHOD(T, fetch_sub, (T, std::memory_order), (noexcept, override));
-    MOCK_METHOD(bool, compare_exchange_strong, (T&, T, std::memory_order, std::memory_order), (noexcept, override));
-    MOCK_METHOD(bool, compare_exchange_strong, (T&, T, std::memory_order), (noexcept, override));
-    MOCK_METHOD(bool, compare_exchange_weak, (T&, T, std::memory_order, std::memory_order), (noexcept, override));
-    MOCK_METHOD(bool, compare_exchange_weak, (T&, T, std::memory_order), (noexcept, override));
-    MOCK_METHOD(void, store, (T, std::memory_order), (noexcept, override));
-    MOCK_METHOD(T, load, (std::memory_order), (noexcept, override));
-};
-
-}  // namespace score::memory::shared
-
-#endif  // SCORE_LIB_MEMORY_SHARED_MOCK_ATOMICMOCK_H
+#endif  // SCORE_MEMORY_SHARED_ATOMIC_MOCK_H
