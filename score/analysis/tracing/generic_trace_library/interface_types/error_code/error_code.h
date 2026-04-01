b96cd23d7e34b178c4b6065fbcf6b0d931472b19
Lorenz Zhao <lorenz.zhao@bmw.de>
2026-04-01 15:57:45 +0200

    GTL: new failure handling concept (without bmw::result)
    GIT_ORIGIN_SPP_REV_ID: e085e708f0550c36365e3033557ae17888c020fe

--- score/analysis/tracing/generic_trace_library/interface_types/error_code/error_code.h
+++ score/analysis/tracing/generic_trace_library/interface_types/error_code/error_code.h
@@ -64,8 +64,6 @@ enum class ErrorCode : score::result::ErrorCode
 
     kDaemonConnectionFailedFatal,             ///< Daemon connection failed (Fatal)
     kDaemonCommunicatorNotSupportedFatal,     ///< Daemon connection is not supported
-    kServerConnectionNameOpenFailedFatal,     ///< QNX name open failed (Fatal)
-    kDaemonTerminationDetectionFailedFatal,   ///< Daemon Termination detection failed (Fatal)
     kClientNotFoundRecoverable,               ///< Client not registered (Recoverable)
     kInvalidShmObjectHandleFatal,             ///< SHM Object handle found (Recoverable)
     kCallbackAlreadyRegisteredRecoverable,    ///< Callback already registered the client id (Recoverable)
@@ -83,9 +81,7 @@ enum class ErrorCode : score::result::ErrorCode
     kNoMoreSpaceForNewClientFatal,                    ///< Max Number of Clients allocated (Fatal)
     kNoMoreSpaceForNewShmObjectFatal,                 ///< Max Number of SHM objects allocated (Fatal)
 
-    kWrongMessageIdRecoverable,         ///< Wrong message id in request (Recoverable)
-    kWrongClientIdRecoverable,          ///< Wrong client id in response (Recoverable)
-    kClientNameAlreadyUsedRecoverable,  // Client name is already used by different process
+    kWrongMessageIdRecoverable,  ///< Wrong message id in request (Recoverable)
 
     kBadFileDescriptorFatal,
 
